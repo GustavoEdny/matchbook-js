893d78cd42240947f58e3cc60adeae747d9bb5b8
gustavo <gustavoedny@gmail.com>
2020-12-21 16:45:27 -0300

    RESTClient.js refactored to be a class instead a object

--- RESTClient.js
+++ RESTClient.js
@@ -2,25 +2,43 @@ const Axios = require('axios')
 
 const client = Axios.create({ baseURL: 'https://api.matchbook.com/' })
 
-const RESTClient = {
-    async get(url, config) {
-        return client.get(url, config = {})
-    },
+class RESTClient {
+    constructor(token = '') {
+        this.token = token
+    }
+
+    static factory(token = '') {
+        return new RESTClient(token)
+    }
+
+    async get(url, config = {}) {
+        return client.get(url, this.buildRequestConfig(config))
+    }
 
     async post(url, data = {}, config = {}) {
-        return client.post(url, data, config)
-    },
+        return client.post(url, data, this.buildRequestConfig(config))
+    }
 
     async put(url, data = {}, config = {}) {
-        return client.put(url, data, config)
-    },
+        return client.put(url, data, this.buildRequestConfig(config))
+    }
 
     async patch(url, data = {}, config = {}) {
-        return client.patch(url, data, config)
-    },
+        return client.patch(url, data, this.buildRequestConfig(config))
+    }
 
     async delete(url, config = {}) {
-        return client.delete(url, config)
+        return client.delete(url, this.buildRequestConfig(config))
+    }
+
+    setToken(token) {
+        this.token = token
+
+        return this
+    }
+
+    buildRequestConfig(config = {}) {
+        return { headers: { 'session-token': this.token }, ...config }
     }
 }
 
